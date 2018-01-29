79d0aec355427da364963941bca39131f3fb36b3
Tong Pow <guitong35@yahoo.com>
2018-01-29 00:27:09 -0500

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,11 @@
+# Implement modulo without using the (%) operator.
+def modulo(a, b):
+    return a - (b * int(a / b))
+
+# Take an input string and determine if exactly 3 question marks
+# exist between every pair of numbers that add up to 10.
+# If so, return true, otherwise return false.
+
+
+def question_mark(s):
+    return True
